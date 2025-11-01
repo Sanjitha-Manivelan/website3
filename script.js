a9e2ab0c0bbc74854cf0767fde883016fb33c977
Sanjitha Manivelan <sanjitha@unknown56bab8ce8e8a.attlocal.net>
2025-10-31 18:25:50 -0700

    working on map for states

--- script.js
+++ script.js
@@ -13,10 +13,625 @@ function displayInfo(event){
 var texasPin = $(".texas");
 var texasInfo = $(".texasInfo");
 
+
 texasPin.on("mouseover", displayInfoTexas);
 texasPin.on("mouseout", displayInfoTexas);
 
 function displayInfoTexas(event){
     event.preventDefault();
     texasInfo.toggleClass("hidden");
+}
+
+
+var floridaPin = $(".florida");
+var floridaInfo = $(".floridaInfo");
+
+
+floridaPin.on("mouseover", displayInfoFlorida);
+floridaPin.on("mouseout", displayInfoFlorida);
+
+function displayInfoFlorida(event){
+    event.preventDefault();
+    floridaInfo.toggleClass("hidden");
+}
+
+var wisconsinPin = $(".wisconsin");
+var wisconsinInfo = $(".wisconsinInfo");
+
+
+wisconsinPin.on("mouseover", displayInfoWisconsin);
+wisconsinPin.on("mouseout", displayInfoWisconsin);
+
+function displayInfoWisconsin(event){
+    event.preventDefault();
+    wisconsinInfo.toggleClass("hidden");
+}
+
+var westvirginiaPin = $(".westvirginia");
+var westvirginiaInfo = $(".westvirginiaInfo");
+
+
+westvirginiaPin.on("mouseover", displayInfoWestvirginia);
+westvirginiaPin.on("mouseout", displayInfoWestvirginia);
+
+function displayInfoWestvirginia(event){
+    event.preventDefault();
+    westvirginiaInfo.toggleClass("hidden");
+}
+
+var virginiaPin = $(".virginia");
+var virginiaInfo = $(".virginiaInfo");
+
+
+virginiaPin.on("mouseover", displayInfoVirginia);
+virginiaPin.on("mouseout", displayInfoVirginia);
+
+function displayInfoVirginia(event){
+    event.preventDefault();
+    virginiaInfo.toggleClass("hidden");
+}
+
+
+
+
+
+var vermontPin = $(".vermont");
+var vermontInfo = $(".vermontInfo");
+
+
+vermontPin.on("mouseover", displayInfoVermont);
+vermontPin.on("mouseout", displayInfoVermont);
+
+function displayInfoVermont(event){
+    event.preventDefault();
+    vermontInfo.toggleClass("hidden");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+california
+newyork
+illinois
+washington
+ohio
+pennsylvania
+michigan
+wyoming
+alabama
+alaska
+arizona
+arkansas
+colorado
+connecticut
+delaware
+hawaii
+idaho
+indiana
+iowa
+kansas
+kentucky
+louisiana
+maine
+maryland
+massachusetts
+minnesota
+mississippi
+missouri
+montana
+nebraska
+nevada
+newhampshire
+newjersey
+newmexico
+northcarolina
+northdakota
+oklahoma
+oregon
+
+
+
+
+
+
+
+
+
+
+
+
+
+var utahPin = $(".utah");
+var utahInfo = $(".utahInfo");
+
+
+utahPin.on("mouseover", displayInfoUtah);
+utahPin.on("mouseout", displayInfoUtah);
+
+function displayInfoUtah(event){
+    event.preventDefault();
+    utahInfo.toggleClass("hidden");
+}
+var tennesseePin = $(".tennessee");
+var tennesseenfo = $(".tennesseeInfo");
+
+
+tennesseePin.on("mouseover", displayInfoTennessee);
+tennesseePin.on("mouseout", displayInfoTennessee);
+
+function displayInfoTennessee(event){
+    event.preventDefault();
+    tennesseeInfo.toggleClass("hidden");
+}
+var southdakotaPin = $(".southdakota");
+var southdakotaInfo = $(".southdakotaInfo");
+
+
+southdakotaPin.on("mouseover", displayInfoSouthdakota);
+southdakotaPin.on("mouseout", displayInfoSouthdakota);
+
+function displayInfoSouthdakota(event){
+    event.preventDefault();
+    southdakotaInfo.toggleClass("hidden");
+}
+var southcarolinaPin = $(".southcarolina");
+var southcarolinaInfo = $(".southcarolinaInfo");
+
+
+southcarolinaPin.on("mouseover", displayInfoSouthcarolina);
+southcarolinaPin.on("mouseout", displayInfoSouthcarolina);
+
+function displayInfoSouthcarolina(event){
+    event.preventDefault();
+    southcarolinaInfo.toggleClass("hidden");
+}
+
+
+var rhodeislandPin = $(".rhodeisland");
+var rhodeislandInfo = $(".rhodeislandInfo");
+
+
+rhodeislandPin.on("mouseover", displayInfoRhodeisland);
+rhodeislandPin.on("mouseout", displayInfoRhodeisland);
+
+function displayInfoRhodeisland(event){
+    event.preventDefault();
+    rhodeislandInfo.toggleClass("hidden");
+}
+
+
+
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
+}
+var Pin = $(".");
+var Info = $(".Info");
+
+
+Pin.on("mouseover", displayInfo);
+Pin.on("mouseout", displayInfo);
+
+function displayInfo(event){
+    event.preventDefault();
+    Info.toggleClass("hidden");
 }
